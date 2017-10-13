98c5a7836e8e67391ca5926c04668afd30f8d621
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-10-13 04:23:07 +0000

    designed landing page

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var campSchema = new mongoose.Schema({
     name: String,
     image: String,
+    price: String,
     description: String,
     author: {
         id: {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,12 +22,13 @@ router.post("/campgrounds", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
+    var price = req.body.price;
     var author = {
         id: req.user._id,
         username: req.user.username
     };
     
-    var newCampground = {name: name, image: image, description: desc, author: author};
+    var newCampground = {name: name, image: image, description: desc, price: price, author: author};
     
     Campground.create(newCampground, function(err, newlyCreated){
         if (err) {
